daf393b4605059a64a2bb800b41f3310fbc9190f
Tahj Anderson <tahjanderson@248.187.21.198.tigernet.wifi.dyn.clemson.edu>
2021-12-01 22:43:54 -0500

    Added the api call to animal API

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello")
+def main():
+    print("Welcome to fish bowl!")
+
+    ## Game starts
+
+    ## User can choose what game mode to choose (Animal words or general words)
+
+    ## Timer starts 
+
+    ## Words start showing up and user controls come into play 
+
+    ## Everytime player passes word new word is called (while loop?)
+
+    ##Game ends , score is showed
+
+    ##User decides to play again or not
+
+    ##End of program
+
+if __name__ == "__main__":
+    main()
