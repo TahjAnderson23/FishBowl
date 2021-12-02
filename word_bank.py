5408d79648655e3b269d2895b71819437c130662
Tyler Piel <44116860+tpiel@users.noreply.github.com>
2021-12-02 10:35:48 -0500

    Get word association api set

--- word_bank.py
+++ word_bank.py
@@ -1,9 +1,25 @@
 import requests 
 
+
 def get_animal_words():
     reponse = requests.get("https://random-word-form.herokuapp.com/random/animal")
     print(reponse.json())
 
+def get_words_like(key):
+    response = requests.get("https://api.datamuse.com/words?ml="+key)
+    word_list = []
+    for i in range(len(response.json())):
+        word_list.append(response.json()[i]['word'])
+    return word_list
     
+def get_words_rhyme(key):
+    response = requests.get("https://api.datamuse.com/words?rel_rhy="+key)
+    word_list = []
+    for i in range(len(response.json())):
+        word_list.append(response.json()[i]['word'])
+    return word_list
 
+bank = get_words_rhyme('apple')
+
+print(bank)
 #get_animal_words()
